31b57fb1ba755cd2e0d73d474aa8482ad7c4cbfa
pro100kryto <pro100kryto.original@gmail.com>
2022-12-25 17:25:28 +0000

    Impl main firstly

--- main/src/main/java/com/japik/env/template/Main.java
+++ main/src/main/java/com/japik/env/template/Main.java
@@ -1,7 +1,57 @@
 package com.japik.env.template;
 
+import com.japik.Japik;
+import com.japik.extension.IExtension;
+import com.japik.extensions.rmiprotocol.shared.IRMIProtocolExtensionConnection;
+import com.japik.networking.Remote;
+
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
+
+        final Japik japik = new Japik(Paths.get(""));
+        japik.getLiveCycle().init();
+        japik.getLiveCycle().start();
+
+        final IExtension<IRMIProtocolExtensionConnection> rmiProtocolExt = (IExtension<IRMIProtocolExtensionConnection>)
+                japik.getExtensionLoader().load("RMIProtocol", "rmiProtocol");
+        rmiProtocolExt.getSettings().put("enableLookup", true);
+        rmiProtocolExt.getSettings().put("port", 13500);
+
+        rmiProtocolExt.getLiveCycle().init();
+        rmiProtocolExt.getLiveCycle().start();
+
+        final Remote remote = japik.getNetworking().getRemoteCollection().add(
+                new Remote.Builder()
+                        .setRemoteName("RMIRemote")
+                        .setProtocolName("RMI")
+        );
+        remote.getProtocolSettings().put("host", "127.0.0.1");
+        remote.getProtocolSettings().put("port", 13500);
+
+        try {
+            remote.getLiveCycle().init();
+            remote.getLiveCycle().start();
+
+            final boolean pingRes = remote.getProtocolInstance().getServiceConnection("rmiProtocol").ping();
+            System.out.println("RMI protocol works! Ping result is: " + pingRes + ".");
+
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        Thread.sleep(150);
+
+        if (remote.getLiveCycle().getStatus().isStarted())
+            remote.getLiveCycle().stopForce();
+        remote.getLiveCycle().destroy();
+
+        rmiProtocolExt.getLiveCycle().stopForce();
+        rmiProtocolExt.getLiveCycle().destroy();
+
+        japik.getLiveCycle().stopForce();
+        japik.getLiveCycle().destroy();
     }
 }
